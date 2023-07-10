dbb4b86b20a8502f43b0fcc7d2306a5a928216a3
fabianc@uazuay.edu.ec <fabianc@uazuay.edu.ec>
2023-07-09 20:05:05 -0500

    primer

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Stage, Layer, Image } from 'react-konva';
+import React, { useState, useRef } from 'react';
+import { Stage, Layer, Image, Line } from 'react-konva';
 import './App.css';
+
+const COLORS = [
+  '#000000', // Black
+  '#FF0000', // Red
+  '#00FF00', // Green
+  '#0000FF', // Blue
+  '#FFFF00', // Yellow
+  '#800080', // Purple
+  '#FFA500', // Orange
+  '#FFC0CB', // Pink
+  '#808080', // Gray
+  '#A52A2A', // Brown
+  '#00FFFF', // Cyan
+  '#FF00FF'  // Magenta
+];
 
 const App = () => {
   const [images, setImages] = useState([]);
-  const [isAnimating, setIsAnimating] = useState(true);
-  const [speed, setSpeed] = useState(1);
-
-  useEffect(() => {
-    let interval;
-
-    if (isAnimating) {
-      interval = setInterval(() => {
-        const newImages = images.map((image) => {
-          let updatedImage = { ...image };
-          updatedImage.y -= 1 * speed;
-
-          if (updatedImage.y <= -250) {
-            updatedImage = {
-              ...updatedImage,
-              y: window.innerHeight,
-              x: Math.random() * (window.innerWidth - 200),
-            };
-          }
-
-          return updatedImage;
-        });
-        setImages(newImages);
-      }, 10);
-    }
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [images, isAnimating, speed]);
+  const [lines, setLines] = useState([]);
+  const [undoHistory, setUndoHistory] = useState([]);
+  const [brushColor, setBrushColor] = useState('#000000');
+  const [brushSize, setBrushSize] = useState(3);
+  const [draggableImages, setDraggableImages] = useState(true);
+  const isDrawing = useRef(false);
+  const linePoints = useRef([]);
 
   const handleDrop = (e) => {
     e.preventDefault();
@@ -43,74 +35,132 @@ const App = () => {
       const img = new window.Image();
       img.src = reader.result;
       img.onload = () => {
-        const newImage = {
-          img: img,
-          x: Math.random() * (window.innerWidth - 200),
-          y: window.innerHeight,
-        };
-
-        const isOverlap = images.some((image) => {
-          const distance = Math.sqrt(
-            Math.pow(newImage.x - image.x, 2) + Math.pow(newImage.y - image.y, 2)
-          );
-          return distance < 250; // Check if the distance is less than the sum of image heights
-        });
-
-        if (!isOverlap) {
-          setImages([...images, newImage]);
-        }
+        setImages([...images, { img, draggable: draggableImages }]);
       };
     };
     reader.readAsDataURL(file);
   };
 
-  const handleToggleAnimation = () => {
-    setIsAnimating(!isAnimating);
+  const handleDragOver = (e) => {
+    e.preventDefault();
   };
 
-  const handleSpeedChange = (e) => {
-    setSpeed(parseFloat(e.target.value));
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const { offsetX, offsetY } = e.evt;
+    linePoints.current = [offsetX, offsetY];
   };
 
-  const handleImageDoubleClick = (index) => {
-    const updatedImages = [...images];
-    updatedImages.splice(index, 1);
-    setImages(updatedImages);
+  const handleMouseMove = (e) => {
+    if (!isDrawing.current) return;
+    const { offsetX, offsetY } = e.evt;
+    const newPoints = [...linePoints.current, offsetX, offsetY];
+    setLines([...lines, { points: newPoints, color: brushColor, size: brushSize }]);
+    linePoints.current = newPoints;
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+    setUndoHistory([]);
+  };
+
+  const handleUndo = () => {
+    if (lines.length === 0) return;
+    const updatedLines = [...lines];
+    const lastLine = updatedLines.pop();
+    setLines(updatedLines);
+    setUndoHistory([...undoHistory, lastLine]);
+  };
+
+  const handleDelete = () => {
+    setLines([]);
+    setUndoHistory([]);
+  };
+
+  const handleRedo = () => {
+    if (undoHistory.length === 0) return;
+    const updatedUndoHistory = [...undoHistory];
+    const lastLine = updatedUndoHistory.pop();
+    setLines([...lines, lastLine]);
+    setUndoHistory(updatedUndoHistory);
+  };
+
+  const handleBrushColorChange = (color) => {
+    setBrushColor(color);
+  };
+
+  const handleBrushSizeChange = (e) => {
+    setBrushSize(Number(e.target.value));
+  };
+
+  const handleToggleDraggable = () => {
+    setDraggableImages(!draggableImages);
+    setImages((prevImages) => {
+      return prevImages.map((image) => {
+        return { ...image, draggable: !draggableImages };
+      });
+    });
+  };
+
+  const renderColorPalette = () => {
+    return COLORS.map((color) => (
+      <div
+        key={color}
+        className={`color-option ${brushColor === color ? 'selected' : ''}`}
+        style={{ background: color }}
+        onClick={() => handleBrushColorChange(color)}
+      ></div>
+    ));
   };
 
   return (
-    <div className="App" onDrop={handleDrop} onDragOver={(e) => e.preventDefault()}>
-      <div className="controls">
-        <button onClick={handleToggleAnimation}>{isAnimating ? 'Detener' : 'Iniciar'}</button>
-        <label htmlFor="speed">Velocidad:</label>
+    <div onDrop={handleDrop} onDragOver={handleDragOver}>
+      <h1>Arrastra y suelta imágenes aquí</h1>
+      <div>
+        <button onClick={handleUndo}>UNDO</button>
+        <button onClick={handleDelete}>DELETE</button>
+        <button onClick={handleRedo}>REDO</button>
+        <button onClick={handleToggleDraggable}>
+          {draggableImages ? 'Disable Drag' : 'Enable Drag'}
+        </button>
+      </div>
+      <div>
+        <div className="color-palette">{renderColorPalette()}</div>
+        <label htmlFor="brush-size">Size:</label>
         <input
           type="range"
-          id="speed"
-          min={0.1}
-          max={10}
-          step={0.1}
-          value={speed}
-          onChange={handleSpeedChange}
+          id="brush-size"
+          min={1}
+          max={9}
+          value={brushSize}
+          onChange={handleBrushSizeChange}
         />
       </div>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
         <Layer>
-          {images.map((image, index) => (
+          {images.map((img, index) => (
             <Image
-              key={index}
-              image={image.img}
-              x={image.x}
-              y={image.y}
-              width={150}
-              height={250}
-              draggable
-              onDragEnd={(e) => {
-                const newImages = [...images];
-                newImages[index].x = e.target.x();
-                newImages[index].y = e.target.y();
-                setImages(newImages);
-              }}
-              onDblClick={() => handleImageDoubleClick(index)}
+              key={`image-${index}`}
+              image={img.img}
+              draggable={img.draggable}
+              onClick={() => handleImageClick(index)}
+              onTap={() => handleImageClick(index)}
+              width={200}
+              height={200}
+            />
+          ))}
+          {lines.map((line, index) => (
+            <Line
+              key={`line-${index}`}
+              points={line.points}
+              stroke={line.color}
+              strokeWidth={line.size}
             />
           ))}
         </Layer>
